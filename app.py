2547a9f8f93f6693f7e58358f47d00b993178df7
iamtheparzival@gmail.com <Disaster@ktm_nepal>
2019-07-09 02:40:44 +0545

    Added the tweet liking and follow option

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import datetime as dt
+
 class TwitterBot():
     def __init__(self,email,password):
         self.email = email
         self.password = password
         self.bot = webdriver.Firefox()
     
+    def logger(self,message):
+        ts = time.time()
+        st = dt.datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
+        file_name = "Log"+st+"_.log"
+        log_file = open(file_name,"a+",errors="ignore")
+        log_file.write(message)
+        log_file.write("\n")
+
     def signin(self):
         bot = self.bot
         bot.get("https://twitter.com/")
 
         #wait some time for the stuff to load
-        time.sleep(5)
+        time.sleep(1)
         email = bot.find_element_by_class_name('email-input')
         password = bot.find_element_by_name('session[password]')
 
@@ -22,6 +32,44 @@ class TwitterBot():
         email.send_keys(self.email)
         password.send_keys(self.password)
         password.send_keys(Keys.RETURN)
+        time.sleep(3)
+
+
+    def hashtag_search(self,hashtag):
+        bot = self.bot
+        #get the webpage by searching with hastags.
+        url = "https://twitter.com/search?q={}&src=typd".format(hashtag)
+        bot.get(url)
+        
+
+    def like_tweet(self,hashtag,option):
+        bot = self.bot
+        self.hashtag_search(hashtag)
+        time.sleep(3)
+        if(option == "l"):
+            class_name = 'HeartAnimation'
+        else:
+            class_name = 'not-following'
+        #now, scroll the page and get the links of tweets.
+        for i in range(1,3):
+            bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+            time.sleep(2)
+            tweets = bot.find_elements_by_class_name('tweet')
+            links = [elem.get_attribute('data-permalink-path')
+                        for elem in tweets]
+
+            #check the options to either follow, like or do both for the given tweet
+            #go through each link and click on the heart animation button to like the tweet.    
+            for link in links:
+                bot.get("https://twitter.com"+link)
+                try:
+                    like = bot.find_element_by_class_name(class_name).click()
+                    time.sleep(5)
+                except Exception as ex:
+                    time.sleep(30)
+                    self.logger(ex)
+            
     
-botter = TwitterBot('harriskunwar@gmail.com','1234')
+botter = TwitterBot('youremail@gmail.com','password')
 botter.signin()
+botter.like_tweet("art",'f')
