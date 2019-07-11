d0ee48c468108d483f131774ac1aef22d0999104
iamtheparzival@gmail.com <Disaster@ktm_nepal>
2019-07-11 16:51:52 +0545

    An option to follow and like at the same time was added, method name fixed.

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ class TwitterBot():
         bot.get(url)
         
 
-    def like_tweet(self,hashtag,option):
+    def like_or_follow(self,hashtag,option):
         bot = self.bot
         self.hashtag_search(hashtag)
         time.sleep(3)
@@ -65,6 +65,34 @@ class TwitterBot():
                 bot.get("https://twitter.com"+link)
                 try:
                     like = bot.find_element_by_class_name(class_name).click()
+                    time.sleep(2)
+                except Exception as ex:
+                    time.sleep(30)
+                    self.logger(ex)
+    
+
+    #Likes and fllows the user at the same tiem with hashtag
+
+    def follow_like(self, hashtag):
+        bot = self.bot
+        self.hashtag_search(hashtag)
+        time.sleep(3)
+        #now, scroll the page and get the links of tweets.
+        for i in range(1, 3):
+            bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+            time.sleep(2)
+            tweets = bot.find_elements_by_class_name('tweet')
+            links = [elem.get_attribute('data-permalink-path')
+                     for elem in tweets]
+
+            #check the options to either follow, like or do both for the given tweet
+            #go through each link and click on the heart animation button to like the tweet.
+            for link in links:
+                bot.get("https://twitter.com"+link)
+                try:
+                    like = bot.find_element_by_class_name('HeartAnimation').click()
+                    time.sleep(2)
+                    follow = bot.find_element_by_class_name('not-following').click()
                     time.sleep(5)
                 except Exception as ex:
                     time.sleep(30)
@@ -75,6 +103,6 @@ class TwitterBot():
         bot.close()
             
     
-botter = TwitterBot('youremail@gmail.com','password')
+botter = TwitterBot('#','#')
 botter.signin()
-botter.like_tweet("art",'f')
+botter.follow_like('tags')
